77234c97e896d863a39cce5294dd10baf2fd31df
ram0973 <ram0973@gmail.com>
2021-08-10 16:57:06 +0300

    add mapstruct

--- src/main/java/com/me/springapp/service/UserDetailsImpl.java
+++ src/main/java/com/me/springapp/service/UserDetailsImpl.java
@@ -33,10 +33,21 @@ public class UserDetailsImpl implements UserDetails {
 
     UserRepository userRepository;
 
+    public UserDetailsImpl() {
+    }
+
+    public UserDetailsImpl(int id, String username, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
     public static UserDetailsImpl build(User user) {
         List<GrantedAuthority> authorities = user.getRoles().stream()
-            .map(role -> new SimpleGrantedAuthority(role.getName().name())
-                .toList());
+            .map(role -> new SimpleGrantedAuthority(role.toString())
+            .toList());
 
         return new UserDetailsImpl(
             user.getId(),
